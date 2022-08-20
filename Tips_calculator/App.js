7ca8b6ea76e5f91d377d66833a7cde262568dd7d
thallyshl <thallys10@gmail.com>
2022-08-20 18:02:10 +0100

    update

--- Tips_calculator/App.js
+++ Tips_calculator/App.js
@@ -1,4 +1,4 @@
-import React, { useStates } from "react";
+import React, { useState } from "react";
 import { View, Text, SafeAreaView, Button, TextInput, StyleSheet } from "react-native";
 
 
@@ -28,7 +28,27 @@ const styles = StyleSheet.create (
       borderWidth: 1,
       paddingStart: 15,
       marginTop: 20,
+      marginBottom: 20,
       borderRadius: 10,
+    },
+    ViewTip: {
+      width: '70%',
+      height: 100,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      marginTop: 100,
+      marginBottom: 150,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    Btn: {
+      margin: 50,
+      flexDirection: 'row',
+    },
+    Result: {
+      fontWeight: 'bold',
+      fontSize: 20,
+
     }
   }
 
@@ -39,10 +59,63 @@ const styles = StyleSheet.create (
 
 
 export default () => {
+
+  const [ bill, setBill ] = useState ('');
+  const [ porc, setPorc ] = useState ('');
+  const [ tip, setTip ] = useState (0);
+  
+  const valueOfTip = () => {
+    let totalBill = parseFloat(bill);
+    let totalPorc = parseFloat(porc);
+       
+    if (totalBill >= 0 && totalPorc >= 0) {
+      setTip ( totalBill * (totalPorc/100) );
+    } else {
+      alert ('Valores inválidos!')
+    }
+    
+  }
+  const clear = () => {
+    setBill ('');
+    setPorc ('');
+    setTip (0);
+  }
+  
+
   return (
     <SafeAreaView style = {styles.Main}>
       <Text style = {styles.Header}> Calculadora de gorjeta </Text>
-      <TextInput placeholder="Qual o valor da conta?" keyboardType="numeric" style = {styles.InputTip}  />
+      <TextInput
+        placeholder="Qual o valor da conta?"
+        keyboardType="numeric"
+        style = {styles.InputTip}
+        onChangeText={n=>setBill(n)}    
+      />
+      <TextInput
+        placeholder="Qual a porcentagem da gorjeta?"
+        keyboardType="numeric"
+        style = {styles.InputTip}
+        onChangeText={p=>setPorc(p)}
+      />
+      { tip > 0 &&
+        <View style = {styles.ViewTip}>
+          <Text style = {styles.Result}>O valor da gorjeta é:</Text>
+          <Text style = {styles.Result}>R$ {tip}</Text>
+        </View>}
+      <View style = {styles.Btn}>
+        <Button
+          title = 'Calcular'
+          style = {styles.Btn}
+          onPress={valueOfTip}
+        />
+
+        <Button
+          title = 'Limpar'
+          style = {styles.Btn}
+          onPress={clear}
+        />
+      </View>  
+      
     </SafeAreaView>
   );
 }
